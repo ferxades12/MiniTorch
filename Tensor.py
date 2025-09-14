d636503cbeeb720dedaaadf72a61c1d9310e8feb
ferxades12 <ferxades12@gmail.com>
2025-09-15 00:58:38 +0200

    Pow autograd and fixes

--- Tensor.py
+++ Tensor.py
@@ -58,6 +58,16 @@ class Tensor:
 
         return result
 
+    def __pow__(self, index):
+        index = index if isinstance(index, Tensor) else Tensor(index)
+
+        op = Pow()
+        result = op.forward(self, index)
+
+        if self.requires_grad:
+            result.grad_fn = op
+
+        return result
 
     def backward(self):
         """
@@ -82,7 +92,7 @@ class Mul:
         result = Tensor(tensor.data * other.data, requires_grad= tensor.requires_grad or other.requires_grad)
         result.is_leaf = False
 
-        self.ctx = [tensor, other]
+        self.ctx = (tensor, other)
 
         return result
 
@@ -93,6 +103,8 @@ class Mul:
         If requires_grad, checks is_leaf
          - If it is True the grad needs to be accumulated in the node
          - If it is False the grad needs to propagated to the node's father
+
+        ax d/dx = a
         """
 
         tensor = self.ctx[0]
@@ -123,7 +135,7 @@ class Add:
         result = Tensor(tensor.data + other.data, requires_grad=tensor.requires_grad or other.requires_grad)
         result.is_leaf = False
 
-        self.ctx = [tensor, other]
+        self.ctx = (tensor, other)
 
         return result
 
@@ -134,6 +146,8 @@ class Add:
         If requires_grad, checks is_leaf
          - If it is True the grad needs to be accumulated in the node
          - If it is False the grad needs to propagated to the node's father
+
+        (a + x) d/dx = 1
         """
 
         tensor = self.ctx[0]
@@ -151,21 +165,75 @@ class Add:
             else:
                 other.grad_fn.backward(grad_output)
 
+class Pow:
+    ctx = None
+    def forward(self, tensor, index):
+        """
+        Creates a Tensor result
+        Saves necessary data in ctx
+        """
+
+        result = Tensor(np.pow(tensor.data, index.data), requires_grad=tensor.requires_grad or index.requires_grad)
+        result.is_leaf = False
+
+        self.ctx = (tensor, index, result.data)
+
+        return result
+
+    def backward(self, grad_output):
+        """
+        Retrieves the data in ctx
+
+        If requires_grad, checks is_leaf
+         - If it is True the grad needs to be accumulated in the node
+         - If it is False the grad needs to propagated to the node's father
+
+        x^a d/dx = a * x^(a-1)
+        a^x d/dx = a^x * ln(a)
+        """
+
+        tensor = self.ctx[0]
+        index = self.ctx[1]
+        result = self.ctx[2]
+
+        tensor_grad = index.data * (np.pow(tensor.data, (index.data - 1))) * grad_output
+        index_grad = result * np.log(tensor.data) * grad_output
+
+        if tensor.requires_grad:
+            if tensor.is_leaf:
+                tensor.grad += tensor_grad
+            else:
+                tensor.grad_fn.backward(tensor_grad)
+
+        if index.requires_grad:
+            if index.is_leaf:
+                index.grad += index_grad
+            else:
+                index.grad_fn.backward(index_grad)
+
 
 def main():
-    x = Tensor([1, 2], requires_grad=True)
-    y = Tensor([3, 4], requires_grad=True)
-    z = Tensor([5, 6], requires_grad=True)
+    # --- TEST COMPLETO ---
+    x = Tensor(2.0, requires_grad=True)
+    y = Tensor(3.0, requires_grad=True)
+    z = Tensor(4.0, requires_grad=True)
 
-    # Combina multiplicación y suma
-    a = x * y  # multiplicación
-    b = a + z  # suma
-    c = b * x  # otra multiplicación
+    # 1️⃣ Multiplicación
+    a = x * y  # a = x * y = 2 * 3 = 6
+
+    # 2️⃣ Suma
+    b = a + z  # b = a + z = 6 + 4 = 10
+
+    # 3️⃣ Potencia
+    c = b ** a  # c = b^a = 10^6 = 1_000_000
+
+    # Retropropagación
     c.backward()
 
-    print("x.grad:", x.grad)
-    print("y.grad:", y.grad)
-    print("z.grad:", z.grad)
+    print("x.grad:", x.grad)  # ≈ 8_707_755
+    print("y.grad:", y.grad)  # ≈ 5_805_170
+    print("z.grad:", z.grad)  # = 600_000
+    print("c:", c.data)  # 1_000_000
 
 
 if __name__ == "__main__":
