9508ecdf8f5a55e2b08f8cd4d4ee18b21eaab1e8
ferxades12 <ferxades12@gmail.com>
2025-10-05 15:48:17 +0200

    Refactor Module class to improve submodule handling and add training state management

--- src/nn/__init__.py
+++ src/nn/__init__.py
@@ -2,3 +2,4 @@ from src.nn.activations import *
 from src.nn.linear import *
 from src.nn.losses import *
 from src.nn.optimizers import *
+from src.nn.regularizations import *
--- src/nn/module.py
+++ src/nn/module.py
@@ -1,33 +1,53 @@
+from src.tensor import Tensor
 
 class Module:
     def __init__(self):
         """Base class for all neural networks and layers.
         """
-        self.submodules = []
+        self.submodules = {}
         self.params = []
+        self.training: bool = True
 
     def __call__(self, *args):
         return self.forward(*args)
+    
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+
+        if isinstance(value, Module):
+            self.submodules[name] = value
 
     def forward(self):
         raise  NotImplementedError
 
-    def parameters(self):
+    def parameters(self) -> list:
         """Return the list of all parameters in the module and its submodules.
         """
         params = self.params
 
-        for module in self.submodules:
+        for module in self.submodules.values():
             params.extend(module.parameters())
 
-        return params 
-    
-    def get_weights(self):
+        return params
+
+    def get_weights(self) -> list[Tensor]:
         weights = []
 
-        for module in self.modules:
+        for module in self.submodules.values():
             weights.extend(module.weight)
 
         return weights
+    
+    def eval(self) -> None:
+        self.training = False
+
+        for module in self.submodules.values():
+            module.training = False
+    
+    def train(self) -> None:
+        self.training = True
+
+        for module in self.submodules.values():
+            module.training = True
 
     
