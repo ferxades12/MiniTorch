0a90434e2eb1e30dafefd3008283e68a38b9194d
ferxades12 <ferxades12@gmail.com>
2025-10-11 17:55:15 +0200

    Refactor add function to ensure proper shape handling and gradient checks

--- src/ops/meta.py
+++ src/ops/meta.py
@@ -1,4 +1,5 @@
 from src.ops import cpu
+import numpy as np
 
 """
 a + b
@@ -14,16 +15,25 @@ dividir si es costosa
 """
 def add(A, B):
     assert A.device == B.device
+    assert hasattr(A, "grad") and hasattr(B, "grad")
 
-    if A.shape() != B.shape():
-        pass #TODO
+    if A.shape != B.shape:
+        A_data, B_data = broadcast(A.data, B.data)
+    else:
+        A_data, B_data = A.data, B.data
 
-    out = A.empty_like()
+    out = np.empty_like(A_data)
 
     if A.device == "cpu":
-        return cpu.add_cpu(A.data, B.data, out)
+        return cpu.add_cpu(A_data, B_data, out)
     elif A.device == "cuda":
         pass
         # kernels.add_kernel(a, b, out)
     else:
         raise NotImplementedError
+
+
+def broadcast(A, B):
+    shape = np.broadcast_shapes(A.shape, B.shape)
+
+    return np.broadcast_to(A, shape), np.broadcast_to(B, shape)
