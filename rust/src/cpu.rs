f647435ea4ac8ee9065bc51aee0ab7f0edb5ce7b
ferxades12 <ferxades12@gmail.com>
2026-01-14 16:47:38 +0100

    sum_cpu() method

--- rust/src/cpu.rs
+++ rust/src/cpu.rs
@@ -1,4 +1,4 @@
-use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD, IxDyn, azip};
+use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD, Axis, IxDyn, arr0, azip};
 
 pub fn add_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
     azip!((out in out, &a in a, &b in b) *out = a + b);
@@ -8,14 +8,17 @@ pub fn mul_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD
     azip!((out in out, &a in a, &b in b) *out = a * b);
 }
 
-fn sub_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
+pub fn sub_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
     azip!((out in out, &a in a, &b in b) *out = a - b);
 }
 
-fn pow_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
+pub fn pow_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
     azip!((out in out, &a in a, &b in b) *out = a.powf(b));
 }
 
-/* fn dot_cpu(a: &ArrayViewD<f32>, b: &ArrayViewD<f32>, out: &mut ArrayViewMutD<f32>){
-    ndarray::linalg::general_mat_mul(1.0, a, b, 0.0, &mut out);
-} */
+pub fn sum_cpu(a: &ArrayViewD<f32>, axis:Option<Axis>, out: &mut ArrayViewMutD<f32>){
+    match axis {
+        None => {out.fill(a.sum());},
+        Some(ax) => {out.assign(&a.sum_axis(ax));}
+    }
+}
