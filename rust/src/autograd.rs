8fa274e39051ec22caf977cdccdcb6665d327d91
ferxades12 <ferxades12@gmail.com>
2026-01-17 23:30:47 +0100

    Refactor backward node implementations to use a macro for consistency and add SubBackward and DivBackward nodes

--- rust/src/autograd.rs
+++ rust/src/autograd.rs
@@ -1,5 +1,5 @@
 use crate::tensor::{Device, Tensor};
-use ndarray::{ArrayD, ArrayViewD, Axis, IxDyn};
+use ndarray::{ArrayD, ArrayViewD, Axis, CowArray, IxDyn};
 
 // Define tanto el enum como el metodo apply
 macro_rules! define_backward_nodes {
@@ -18,47 +18,72 @@ macro_rules! define_backward_nodes {
     };
 }
 
-define_backward_nodes!(AddBackward, MulBackward, SumBackward);
+define_backward_nodes!(AddBackward, MulBackward, SubBackward, DivBackward, SumBackward);
 
 pub trait Backward {
     fn apply(&self, grad_output: ArrayViewD<f32>);
 }
 
-pub struct AddBackward {
-    pub tensor: Tensor,
-    pub other: Tensor,
-}
+macro_rules! implement_bitwise_backward {
+    /*
+       $name : Nombre del nodo a implementar (AddBackward)
+       $funca : funcion que calcula el gradiente de tensor
+       $funcb : funcion que calcula el gradiente de other
+       El resultado debe ser ArrayD o ArrayViewD, que posteriormente se pasara a CowArray en la macro
+    */
+    ($name:ident, $funca:expr , $funcb:expr ) => {
+        pub struct $name {
+            pub tensor: Tensor,
+            pub other: Tensor,
+        }
 
-impl Backward for AddBackward {
-    fn apply(&self, grad_output: ArrayViewD<f32>) {
-        _update_grad(&self.tensor, grad_output.clone());
-        _update_grad(&self.other, grad_output);
-    }
-}
+        impl Backward for $name {
+            fn apply(&self, grad_output: ArrayViewD<f32>) {
+                let (grad_a, grad_b) = match (&*self.tensor.data, &*self.other.data) {
+                    (Device::CPU(data_a), Device::CPU(data_b)) => (
+                        CowArray::from($funca(data_a, data_b, grad_output.clone())),
+                        CowArray::from($funcb(data_a, data_b, grad_output)),
+                    ),
+                    _ => {
+                        panic!(
+                            "Combinación de dispositivos no soportada: ({:?}, {:?})",
+                            &*self.tensor.data, &*self.other.data
+                        );
+                    }
+                };
 
-pub struct MulBackward {
-    pub tensor: Tensor,
-    pub other: Tensor,
-}
-
-impl Backward for MulBackward {
-    fn apply(&self, grad_output: ArrayViewD<f32>) {
-        let grad_a = match &*self.other.data {
-            Device::CPU(data_b) => &grad_output * data_b,
-            _ => panic!("Device not implemented"),
-        };
-
-        let grad_b = match &*self.tensor.data {
-            Device::CPU(data_a) => data_a * &grad_output,
-            _ => {
-                panic!("Device not supported")
+                _update_grad(&self.tensor, grad_a);
+                _update_grad(&self.other, grad_b);
             }
-        };
-
-        _update_grad(&self.tensor, grad_a.view());
-        _update_grad(&self.other, grad_b.view());
-    }
+        }
+    };
 }
+
+implement_bitwise_backward!(
+    AddBackward,
+    |_data_a, _data_b, grad_output| grad_output,
+    |_data_a, _data_b, grad_output| grad_output
+);
+
+implement_bitwise_backward!(
+    MulBackward,
+    |_data_a, data_b, grad_output| &grad_output * data_b,
+    |data_a, _data_b, grad_output| data_a * &grad_output
+);
+
+implement_bitwise_backward!(
+    SubBackward,
+    |_data_a, _data_b, grad_output| grad_output,
+    |_data_a, _data_b, grad_output: ArrayViewD<f32>| -&grad_output
+);
+
+implement_bitwise_backward!(
+    DivBackward,
+    |_data_a, data_b, grad_output| &grad_output / data_b,
+    |data_a: &ArrayD<f32>, data_b: &ArrayD<f32>, grad_output| {
+        (-data_a / data_b.mapv(|a| a * a)) * grad_output
+    }
+);
 
 pub struct SumBackward {
     pub tensor: Tensor,
@@ -71,9 +96,7 @@ impl Backward for SumBackward {
         let expanded;
 
         let grad = match self.axis {
-            None => grad_output
-                .broadcast(IxDyn(&self.tensor.shape))
-                .expect("Error en sum() total"),
+            None => grad_output.broadcast(IxDyn(&self.tensor.shape)).expect("Error en sum() total"),
             Some(ax) => {
                 expanded = grad_output.insert_axis(ax);
                 expanded
@@ -82,11 +105,11 @@ impl Backward for SumBackward {
             }
         };
 
-        _update_grad(&self.tensor, grad);
+        _update_grad(&self.tensor, CowArray::from(grad));
     }
 }
 
-fn _update_grad(tensor: &Tensor, grad: ArrayViewD<f32>) {
+fn _update_grad(tensor: &Tensor, grad: CowArray<f32, IxDyn>) {
     if !tensor.requires_grad {
         return;
     }
@@ -103,15 +126,15 @@ fn _update_grad(tensor: &Tensor, grad: ArrayViewD<f32>) {
         match grad_lock.as_mut() {
             // la impl del trait addasign esta definido como *a += &view, y no toma ownership de view
             Some(tensor_grad) => {
-                *tensor_grad += &grad;
+                *tensor_grad += &grad.view();
             }
             None => {
-                *grad_lock = Some(grad.to_owned());
+                *grad_lock = Some(grad.into_owned());
             }
         }
     } else {
         //tensor.grad_fn.backward(grad);
 
-        tensor._backward(Some(grad));
+        tensor._backward(Some(grad.view()));
     }
 }
